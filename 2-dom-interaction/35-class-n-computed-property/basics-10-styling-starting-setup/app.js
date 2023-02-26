23a0af140ac43a3bd2653ae474249ce9277b71ce
echoedinvoker <echoedinvoker@gmail.com>
2023-02-26 16:01:39 +0800

    qq

---
+++ 2-dom-interaction/35-class-n-computed-property/basics-10-styling-starting-setup/app.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      selectedBoxA: false,
+      selectedBoxB: false,
+      selectedBoxC: false,
+    };
+  },
+  computed: {
+    boxAclasses() {
+      return { active: this.selectedBoxA };
+    },
+  },
+  methods: {
+    reverseFlag(box) {
+      if (box === "A") {
+        this.selectedBoxA = !this.selectedBoxA;
+      } else if (box === "B") {
+        this.selectedBoxB = !this.selectedBoxB;
+      } else if (box === "C") {
+        this.selectedBoxC = !this.selectedBoxC;
+      }
+    },
+  },
+});
+
+app.mount("#styling");
---
+++ 2-dom-interaction/35-class-n-computed-property/memo.md
@@ -0,0 +1,9 @@
+## **Isn't HTML not allowed to have logic?**
+
+![Alt move logic out of HTML](pic/06.jpg)
+
+- It is allowed to write in HTML if it is just a simple boolean to set the on/off of each class.
+  - Can make the code more concise
+- But if the logic is more complex, it must be written in 'compute'.
+
+![Alt move logic into computed](pic/07.jpg)
