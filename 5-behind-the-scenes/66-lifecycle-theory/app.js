a16e15db44028f1745a1a5c18ba9e8e4b5cb2df9
echoedinvoker <echoedinvoker@gmail.com>
2023-02-28 14:40:03 +0800

    qq

---
+++ 5-behind-the-scenes/66-lifecycle-theory/app.js
@@ -0,0 +1,30 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      // currentUserInput: "",
+      message: "Vue is great!",
+    };
+  },
+  methods: {
+    // saveInput(event) {
+    //   this.currentUserInput = event.target.value;
+    // },
+    setText() {
+      // this.message = this.currentUserInput;
+      this.message = this.$refs.userText.value;
+      console.log(this.$refs.userText);
+    },
+  },
+});
+
+app.mount("#app");
+
+const app2 = Vue.createApp({
+  data() {
+    return {
+      favoriteMeal: "pizza",
+    };
+  },
+});
+
+app2.mount("#app2");
