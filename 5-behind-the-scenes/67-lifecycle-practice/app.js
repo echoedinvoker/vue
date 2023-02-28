75f26b059d67b5ea4482517a5f2a0c2ec716eff2
echoedinvoker <echoedinvoker@gmail.com>
2023-02-28 19:19:43 +0800

    qq

---
+++ 5-behind-the-scenes/67-lifecycle-practice/app.js
@@ -0,0 +1,43 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      message: "Vue is great!",
+    };
+  },
+  methods: {
+    setText() {
+      this.message = this.$refs.userText.value;
+      console.log(this.$refs.userText);
+    },
+  },
+  beforeCreate() {
+    console.log("beforeCreate()");
+  },
+  created() {
+    console.log("created()");
+  },
+  beforeMount() {
+    console.log("beforeMount()");
+  },
+  mounted() {
+    console.log("mounted()");
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate()");
+  },
+  updated() {
+    console.log("updated()");
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount()");
+  },
+  unmounted() {
+    console.log("unmounted()");
+  },
+});
+
+app.mount("#app");
+
+setTimeout(() => {
+  app.unmount();
+}, 5000);
---
+++ 5-behind-the-scenes/67-lifecycle-practice/memo.md
@@ -0,0 +1,23 @@
+## **createApp({...})**
+
+![Alt lifecyle after createApp concept chart](pic/01.jpg)
+
+![Alt use methods and set breakpoint](pic/02.jpg)
+
+![Alt mounted()](pic/03.jpg)
+
+## **Data Changed**
+
+![Alt lifecycle after data changed concept chart](pic/04.jpg)
+
+![Alt use methods and set breakpoint](pic/05.jpg)
+
+![Alt updated()](pic/06.jpg)
+
+## **Unmounted**
+
+![Alt lifecycle after unmounted concept chart](pic/07.jpg)
+
+![Alt use methods and set breakpoint](pic/08.jpg)
+
+![Alt unmounted()](pic/09.jpg)
