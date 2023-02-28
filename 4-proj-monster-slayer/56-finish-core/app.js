d8fa51f6b065bc17f16eb8e703f452026aa5b8ca
echoedinvoker <echoedinvoker@gmail.com>
2023-02-28 08:10:33 +0800

    qq

---
+++ 4-proj-monster-slayer/56-finish-core/app.js
@@ -0,0 +1,86 @@
+const getAttackValue = function (max, min) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const app = Vue.createApp({
+  data() {
+    return {
+      monsterHealth: 100,
+      playerHealth: 100,
+      playRound: 0,
+      winner: null,
+    };
+  },
+  computed: {
+    monsterHealthWidth() {
+      if (this.monsterHealth < 0) {
+        return { width: "0%" };
+      }
+      return { width: this.monsterHealth + "%" };
+    },
+    playerHealthWidth() {
+      return { width: this.playerHealth + "%" };
+    },
+    maySpecialAttackAvailable() {
+      return this.playRound % 3 !== 0 || this.playRound === 0;
+    },
+  },
+  methods: {
+    startNewGame() {
+      this.monsterHealth = 100;
+      this.playerHealth = 100;
+      this.playRound = 0;
+      this.winner = null;
+    },
+    playerAttack() {
+      this.playRound++;
+      const attackValue = getAttackValue(12, 5);
+      if (this.monsterHealth - attackValue < 0) {
+        this.monsterHealth = 0;
+      } else {
+        this.monsterHealth -= attackValue;
+      }
+      this.monsterAttack();
+    },
+    playerSpecialAttack() {
+      this.playRound++;
+      const attackValue = getAttackValue(20, 10);
+      if (this.monsterHealth - attackValue < 0) {
+        this.monsterHealth = 0;
+      } else {
+        this.monsterHealth -= attackValue;
+      }
+      this.monsterAttack();
+    },
+    playerHeal() {
+      this.playRound++;
+      const healValue = getAttackValue(20, 10);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      setTimeout(this.monsterAttack, 500);
+    },
+    playerSurrender() {
+      this.winner = "monster";
+    },
+    monsterAttack() {
+      const attackValue = getAttackValue(15, 8);
+      if (this.playerHealth - attackValue < 0) {
+        this.playerHealth = 0;
+      } else {
+        this.playerHealth -= attackValue;
+      }
+      if (this.playerHealth === 0 && this.monsterHealth === 0) {
+        this.winner = "draw";
+      } else if (this.playerHealth === 0) {
+        this.winner = "monster";
+      } else if (this.monsterHealth === 0) {
+        this.winner = "player";
+      }
+    },
+  },
+});
+
+app.mount("#game");
---
+++ 4-proj-monster-slayer/56-finish-core/memo.md
@@ -0,0 +1,17 @@
+## **Fix Low Health Stuck Issue in another way**
+
+![Alt fix health stuck in another way](pic/06.jpg)
+
+## **Start New Game**
+
+![Alt restart game](pic/07.jpg)
+
+## **Avoid Any Actions After GAME OVER**
+
+![Alt still action after game over](pic/08.jpg)
+
+![Alt simply v-else](pic/09.jpg)
+
+## **Surrender**
+
+![Alt surrender](pic/10.jpg)
