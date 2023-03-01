363ee329c45c0554e6a131682e4856cbece40e29
echoedinvoker <echoedinvoker@gmail.com>
2023-03-02 05:20:57 +0800

    qq

---
+++ 7-setup-cli/85-basice-app/memo.md
@@ -0,0 +1,13 @@
+## **App.vue & script export/import**
+
+![Alt App.vue and script](pic/01.jpg)
+
+## **template**
+
+![Alt template](pic/02.jpg)
+
+## **page and error**
+
+![Alt error on page](pic/03.jpg)
+
+![Alt fix and page empty](pic/04.jpg)
---
+++ project/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <h3>Friends</h3>
+  <ul>
+    <li v-for="friend in friends" :key="friend.id">
+      <p><strong>Name:</strong> {{ friend.name }}</p>
+      <p><strong>Phone:</strong> {{ friend.phone }}</p>
+      <p><strong>Email:</strong> {{ friend.email }}</p>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "1234 567 890",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "0978 567 890",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
