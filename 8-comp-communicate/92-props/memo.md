35a521f191debb42b31ac488c15b8c3142796a53
echoedinvoker <echoedinvoker@gmail.com>
2023-03-02 09:46:20 +0800

    qq

---
+++ 8-comp-communicate/92-props/memo.md
@@ -0,0 +1,15 @@
+## **Component Problem & Purpose**
+
+![Alt problem](pic/03.jpg)
+
+![Alt purpose](pic/04.jpg)
+
+## **Try & Solution - 'props'**
+
+![Alt try](pic/05.jpg)
+
+![Alt props option](pic/06.jpg)
+
+- 'props' means 'properties'.
+
+![Alt result](pic/07.jpg)
---
+++ 8-comp-communicate/cmp-communication-01-starting-setup/src/components/FriendContact.vue
@@ -0,0 +1,43 @@
+<template>
+  <li>
+    <!-- <h2>{{ friend.name }}</h2> -->
+    <h2>{{ friendName }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        <!-- {{ friend.phone }} -->
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        <!-- {{ friend.email }} -->
+        {{ emailAddress }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["friendName", "phoneNumber", "emailAddress"],
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "0123 45678 90",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
