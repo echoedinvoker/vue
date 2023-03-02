6717f830b04bd4cc07192c684a3310c428b1a669
echoedinvoker <echoedinvoker@gmail.com>
2023-03-02 08:56:57 +0800

    qq

---
+++ 7-setup-cli/87-add-style/memo.md
@@ -0,0 +1,7 @@
+## **Style Block of Vue File**
+
+![Alt optional block style](pic/01.jpg)
+
+## **Copy Paste Styles of Previous Lectures into it**
+
+![Alt copy paste before style to it](pic/02.jpg)
--- project/vue-cli-01-a-new-vue-project/src/main.js
+++ project/vue-cli-01-a-new-vue-project/src/main.js
@@ -3,7 +3,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import FriendContact from "./component/FriendContact.vue";
 
-// createApp(App).mount("#app");
 const app = createApp(App);
 
 app.component("friend-contact", FriendContact);
