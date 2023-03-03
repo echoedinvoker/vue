d4571b91ce072fad7d23a815df44b9b7c0a7a6c9
echoedinvoker <echoedinvoker@gmail.com>
2023-03-03 08:54:19 +0800

    qq

---
+++ 8-comp-communicate/104-provdie-inject-for-func/memo.md
@@ -0,0 +1,7 @@
+## **Issue: Can function/methods be provide/inject?**
+
+![Alt problem](pic/01.jpg)
+
+## **Provide/Inject function/methods**
+
+![Alt provide fun](pic/02.jpg)
--- 8-comp-communicate/cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeGrid.vue
+++ 8-comp-communicate/cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeGrid.vue
@@ -1,20 +1,26 @@
 <template>
   <ul>
-    <knowledge-element
+    <!-- <knowledge-element
       v-for="topic in topics"
       :key="topic.id"
       :id="topic.id"
       :topic-name="topic.title"
       :description="topic.description"
       @select-topic="$emit('select-topic', $event)"
+    ></knowledge-element> -->
+    <knowledge-element
+      v-for="topic in topics"
+      :key="topic.id"
+      :id="topic.id"
+      :topic-name="topic.title"
+      :description="topic.description"
     ></knowledge-element>
   </ul>
 </template>
 
 <script>
 export default {
-  // props: ['topics'],
   inject: ['topics'],
-  emits: ['select-topic'],
+  // emits: ['select-topic'],
 };
 </script>
