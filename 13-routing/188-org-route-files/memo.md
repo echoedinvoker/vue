17f8d2dc6a77b78b02d7f7d21454cc3fdb98dc9d
echoedinvoker <echoedinvoker@gmail.com>
2023-03-08 06:34:37 +0800

    qq

---
+++ 13-routing/188-org-route-files/memo.md
@@ -0,0 +1,18 @@
+## **Split Page/normal components**
+
+![Alt page components](pic/01.jpg)
+
+![Alt split page and normal components](pic/02.jpg)
+
+- This is optional.
+  - Suitable for large projects.
+  - No split is fine, this means that the page component needs to be used as a normal component as well, which is more flexible. but hard to maintain.
+    - In this case, make sure that the components of the route are configured props=true.
+
+## **Split router codes from main.js**
+
+> router itself is a complete topic and object, stored in a separate file is very reasonable.
+
+![Alt router codes in main.js](pic/03.jpg)
+
+![Alt splite router codes to specific file](pic/04.jpg)
--- 13-routing/routing-01-starting-setup/src/main.js
+++ 13-routing/routing-01-starting-setup/src/main.js
@@ -1,61 +1,7 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
-import TeamsList from './components/teams/TeamsList.vue';
-import UsersList from './components/users/UsersList.vue';
-import TeamMembers from './components/teams/TeamMembers.vue';
-import NotFound from './components/nav/NotFound.vue';
-import TeamsFooter from './components/teams/TeamsFooter.vue';
-import UsersFooter from './components/users/UsersFooter.vue';
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', redirect: '/teams' },
-    {
-      path: '/teams',
-      components: {
-        default: TeamsList,
-        footer: TeamsFooter,
-      },
-      children: [
-        {
-          name: 'team-member',
-          path: '/teams/:teamId',
-          component: TeamMembers,
-          props: true,
-        },
-      ],
-      meta: {
-        needsAuth: true,
-      },
-    },
-    {
-      path: '/users',
-      components: {
-        default: UsersList,
-        footer: UsersFooter,
-      },
-    },
-    { path: '/:notFound(.*)', component: NotFound },
-  ],
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition;
-    }
-    return { left: 0, top: 0 };
-  },
-});
-
-router.beforeEach((to, from, next) => {
-  if (to.meta.needsAuth) {
-    // check user auth
-    next('true or false');
-  } else {
-    next();
-  }
-});
+import router from './router.js';
 
 const app = createApp(App);
 
