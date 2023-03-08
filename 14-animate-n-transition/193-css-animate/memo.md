36cbd759e047c605244a9c2c25567655c11f83f6
echoedinvoker <echoedinvoker@gmail.com>
2023-03-08 09:38:14 +0800

    qq

---
+++ 14-animate-n-transition/193-css-animate/memo.md
@@ -0,0 +1,11 @@
+## **Second way of animation: @keyframes**
+
+![Alt @keyframes](pic/07.jpg)
+
+## **Issue: back to original status**
+
+![Alt problem](pic/08.jpg)
+
+![Alt prevent back to original](pic/09.jpg)
+
+![Alt result](pic/10.jpg)
--- 14-animate-n-transition/animations-01-starting-setup/src/App.vue
+++ 14-animate-n-transition/animations-01-starting-setup/src/App.vue
@@ -60,7 +60,7 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
-  transition: transform 1s ease-out;
+  /* transition: transform 1s ease-out; */
 }
 .container {
   max-width: 40rem;
@@ -74,6 +74,20 @@ button:active {
   border-radius: 12px;
 }
 .animate {
-  transform: translateX(-150px);
+  /* transform: translateX(-150px); */
+  animation: side-fade 0.3s ease-out forwards;
+}
+@keyframes side-fade {
+  0% {
+    transform: translateX(0) scale(1);
+  }
+
+  70% {
+    transform: translateX(-120px) scale(1.1);
+  }
+
+  100% {
+    transform: translate(-150px) scale(1);
+  }
 }
 </style>
