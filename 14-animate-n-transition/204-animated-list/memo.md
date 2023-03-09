9788e5f53395692d9adec1dd26aa12e83fc2502d
echoedinvoker <echoedinvoker@gmail.com>
2023-03-09 15:16:59 +0800

    qq

---
+++ 14-animate-n-transition/204-animated-list/memo.md
@@ -0,0 +1,15 @@
+# **\*Prepare Functional List**
+
+> This lecture prepares a simple unordered list with the ability to add and remove members for the later lectures to explain how to add animations to these operations.
+
+## **Create a Simple Unorder List**
+
+![Alt create a list](pic/01.jpg)
+
+## **Simple Styling to it**
+
+![Alt add simple style](pic/02.jpg)
+
+## **Add ways to add/remove users**
+
+![Alt add/remove user func](pic/03.jpg)
---
+++ 14-animate-n-transition/animations-01-starting-setup/src/components/ListData.vue
@@ -0,0 +1,43 @@
+<template>
+  <ul>
+    <li v-for="user in users" :key="user" @click="removeUser(user)">
+      {{ user }}
+    </li>
+  </ul>
+  <input type="text" ref="enteredUser" />
+  <button type="button" @click="inputUser">Input User</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: ['Matt', 'Justin', 'Philip', 'Jocelyn'],
+    };
+  },
+  methods: {
+    removeUser(user) {
+      this.users = this.users.filter((usr) => usr !== user);
+    },
+    inputUser() {
+      if (this.$refs.enteredUser.value.trim()) {
+        this.users.unshift(this.$refs.enteredUser.value);
+        this.$refs.enteredUser.value = '';
+      }
+    },
+  },
+};
+</script>
+
+<style>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+li {
+  border: 1px solid #ccc;
+  padding: 1rem;
+  text-align: center;
+}
+</style>
