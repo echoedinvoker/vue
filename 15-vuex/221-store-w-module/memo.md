023b4e5d9269fb772e11d88048d9f64d21e9bc69
echoedinvoker <echoedinvoker@gmail.com>
2023-03-11 04:59:16 +0800

    qq

---
+++ 15-vuex/221-store-w-module/memo.md
@@ -0,0 +1,17 @@
+## **Problem: Vue Store getting fat**
+
+![Alt problem](pic/01.jpg)
+
+## **Vue Module**
+
+### _Structure_
+
+![Alt module structure](pic/02.jpg)
+
+### _Split contents from store to module_
+
+![Alt splite store contents to module](pic/03.jpg)
+
+## **Page Result**
+
+![Alt page](pic/04.jpg)
--- 15-vuex/vuex-01-starting-setup/src/main.js
+++ 15-vuex/vuex-01-starting-setup/src/main.js
@@ -1,28 +1,18 @@
 import { createApp } from 'vue';
 import { createStore } from 'vuex';
 
-const store = createStore({
+const counterModule = {
   state() {
     return {
       counter: 0,
-      authenticated: false,
     };
   },
-  mutations: {
+  mutaions: {
     increment(state) {
       state.counter++;
     },
     increase(state, payload) {
       state.counter = state.counter + payload.value;
-    },
-    // login(state) {
-    //   state.authenticated = true;
-    // },
-    // logout(state) {
-    //   state.authenticated = false;
-    // },
-    setAuth(state, payload) {
-      state.authenticated = payload.isAuth;
     },
   },
   actions: {
@@ -35,19 +25,8 @@ const store = createStore({
       console.log(context);
       context.commit('increase', payload);
     },
-    login(context) {
-      // context.commit('login');
-      context.commit('setAuth', { isAuth: true });
-    },
-    logout(context) {
-      // context.commit('logout');
-      context.commit('setAuth', { isAuth: false });
-    },
   },
   getters: {
-    authenticated(state) {
-      return state.authenticated;
-    },
     finalCounter(state) {
       return state.counter * 4;
     },
@@ -61,6 +40,65 @@ const store = createStore({
       }
       return finalCounter;
     },
+  },
+};
+
+const store = createStore({
+  modules: {
+    numbers: counterModule,
+  },
+  state() {
+    return {
+      // counter: 0,
+      authenticated: false,
+    };
+  },
+  mutations: {
+    // increment(state) {
+    //   state.counter++;
+    // },
+    // increase(state, payload) {
+    //   state.counter = state.counter + payload.value;
+    // },
+    setAuth(state, payload) {
+      state.authenticated = payload.isAuth;
+    },
+  },
+  actions: {
+    // increment(context) {
+    //   setTimeout(function () {
+    //     context.commit('increment');
+    //   }, 2000);
+    // },
+    // increase(context, payload) {
+    //   console.log(context);
+    //   context.commit('increase', payload);
+    // },
+    login(context) {
+      // context.commit('login');
+      context.commit('setAuth', { isAuth: true });
+    },
+    logout(context) {
+      context.commit('setAuth', { isAuth: false });
+    },
+  },
+  getters: {
+    authenticated(state) {
+      return state.authenticated;
+    },
+    // finalCounter(state) {
+    //   return state.counter * 4;
+    // },
+    // rangeCounter(_, getters) {
+    //   const finalCounter = getters.finalCounter;
+    //   if (finalCounter < 0) {
+    //     return 0;
+    //   }
+    //   if (finalCounter > 100) {
+    //     return 100;
+    //   }
+    //   return finalCounter;
+    // },
   },
 });
 
