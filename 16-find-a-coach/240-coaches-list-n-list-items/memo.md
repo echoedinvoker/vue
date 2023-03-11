190a4f53abb53c6c6a41d739b4b22bd3a58df29d
echoedinvoker <echoedinvoker@gmail.com>
2023-03-12 07:39:02 +0800

    qq

---
+++ 16-find-a-coach/240-coaches-list-n-list-items/memo.md
@@ -0,0 +1,20 @@
+## **Plan**
+
+![Alt plan and file/folder](pic/01.jpg)
+
+- Technically we could implement everything in the CoachList, but it would be better to use additional component spliting codes.
+  - This component belongs to the normal component.
+
+## **CoachItem**
+
+![Alt create list component](pic/02.jpg)
+
+### _computed_
+
+![Alt computed links](pic/03.jpg)
+
+## **Replace <li> with CoachItem**
+
+![Alt use list component in ul component](pic/04.jpg)
+
+![Alt result and future plan](pic/05.jpg)
--- 16-find-a-coach/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ 16-find-a-coach/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -6,17 +6,30 @@
       <router-link to="/register">Register as Coach</router-link>
     </div>
     <ul v-if="hasCoaches">
-      <!-- LIST OF COACHES -->
-      <li v-for="coach in filteredCoaches" :key="coach.id">
+      <!-- <li v-for="coach in filteredCoaches" :key="coach.id">
         {{ coach.firstName }}
-      </li>
+      </li> -->
+      <coach-item
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :firstName="coach.firstName"
+        :lastName="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas"
+      ></coach-item>
     </ul>
     <h3 v-else>No coaches found.</h3>
   </section>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
+
 export default {
+  components: {
+    CoachItem,
+  },
   computed: {
     filteredCoaches() {
       return this.$store.getters['coaches/coaches'];
@@ -27,3 +40,16 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
