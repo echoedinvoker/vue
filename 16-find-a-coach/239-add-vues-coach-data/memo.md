4c7c9d546171d0bd357407e90bcb1ff7a7c44f8c
echoedinvoker <echoedinvoker@gmail.com>
2023-03-12 06:27:55 +0800

    qq

---
+++ 16-find-a-coach/239-add-vues-coach-data/memo.md
@@ -0,0 +1,21 @@
+## **Create Vue Store Files/Folders**
+
+![Alt files](pic/01.jpg)
+
+## **Build Basic Structure**
+
+![Alt basic structure](pic/02.jpg)
+
+![Alt connect to mainjs](pic/03.jpg)
+
+## **Import Dummy Data & Getters**
+
+![Alt import dummy data](pic/04.jpg)
+
+![Alt getters](pic/05.jpg)
+
+## **Usage in the Compoenent**
+
+![Alt use in component](pic/06.jpg)
+
+![Alt no coach](pic/07.jpg)
--- 16-find-a-coach/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ 16-find-a-coach/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -5,8 +5,25 @@
       <button>Refresh</button>
       <router-link to="/register">Register as Coach</router-link>
     </div>
-    <ul>
-      LIST OF COACHES
+    <ul v-if="hasCoaches">
+      <!-- LIST OF COACHES -->
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+        {{ coach.firstName }}
+      </li>
     </ul>
+    <h3 v-else>No coaches found.</h3>
   </section>
 </template>
+
+<script>
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+};
+</script>
