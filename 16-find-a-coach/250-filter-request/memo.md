d05031f7bf0333e2df9856483abd2cf46abe702e
echoedinvoker <echoedinvoker@gmail.com>
2023-03-14 16:14:57 +0800

    qq

---
+++ 16-find-a-coach/250-filter-request/memo.md
@@ -0,0 +1,17 @@
+## **Direct filtering with getters**
+
+> Filter the data that can be given to the logged-in user based on the user, which is suitable for filtering directly in the getters.
+
+![Alt refactor getters of requests](pic/06.jpg)
+
+## **Test**
+
+![Alt test - register](pic/07.jpg)
+
+![Alt test - register form](pic/08.jpg)
+
+![Alt test - click contact](pic/09.jpg)
+
+![Alt test - contact form](pic/10.jpg)
+
+![Alt test - result](pic/11.jpg)
--- 16-find-a-coach/main-prj-01-starting-setup/src/store/requests/getters.js
+++ 16-find-a-coach/main-prj-01-starting-setup/src/store/requests/getters.js
@@ -1,8 +1,13 @@
 export default {
-  requests(state) {
-    return state.requests;
+  // requests(state) {
+  requests(state, _, _2, rootGetters) {
+    // return state.requests;
+    const coachId = rootGetters.userId;
+    // return state.requests;
+    return state.requests.filter((req) => req.coachId === coachId);
   },
-  hasRequests(state) {
-    return state.requests && state.requests.length > 0;
+  hasRequests(_, getters) {
+    // return state.requests && state.requests.length > 0;
+    return getters.requests && getters.requests.length > 0;
   },
 };
