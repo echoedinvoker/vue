9bfa4e41b3e1fda9efdb65896603e47eb39929ae
echoedinvoker <echoedinvoker@gmail.com>
2023-03-15 15:41:30 +0800

    qq

---
+++ 19-composition/289-replace-methods/memo.md
@@ -0,0 +1,6 @@
+## **Replace 'methods'**
+
+![Alt methods](pic/01.jpg)
+
+- The difference is that we don't use 'this' in setup().
+- When using a ref object, remember that the value is wrapped in the 'value' property.
--- 19-composition/composition-01-starting-setup/src/App.vue
+++ 19-composition/composition-01-starting-setup/src/App.vue
@@ -1,43 +1,28 @@
 <template>
   <section class="container">
-    <h2>{{ userName }}</h2>
-    <!-- <h2>{{ userAge }}</h2> -->
-    <!-- <h2>{{ user.userName }}</h2> -->
+    <h2>{{ user.userName }}</h2>
     <h2>{{ user.userAge }}</h2>
+    <button @click="addAge">add age</button>
   </section>
 </template>
 
 <script>
-import { ref } from 'vue';
 import { reactive } from 'vue';
 
 export default {
   setup() {
-    const userName = ref('Maximilian');
-    // const userAge = ref(31);
-    // const user = ref({
     const user = reactive({
-      // userName: 'Maximilian',
+      userName: 'Maximilian',
       userAge: 31,
     });
 
-    setTimeout(() => {
-      userName.value = 'Max';
-      // userAge.value = 32;
-      // user.value.userName = 'Max';
-      // user.value.userAge = 32;
-      // user.userName = 'Max';
-      user.userAge = 32;
-    }, 2000);
-
-    console.log(userName);
-    console.log(user);
+    function addAge() {
+      user.userAge += 1;
+    }
 
     return {
-      userName,
-      // userName: user.value.userName,
-      // userAge: user.value.userAge,
       user,
+      addAge,
     };
   },
 };
